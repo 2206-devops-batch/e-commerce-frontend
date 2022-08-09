43016e64b415146a7df9afe73ec2caec4cac859b
Christopher Barnes <chris.barnes.2000@me.com>
2022-08-09 11:55:30 -0700

    [Update]: CrossOrigin

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'axios'
 
 // This is the configuration for sending HTTP Requests with Axios
 // Very simple, but it also doesn't give us much abstraction
@@ -7,13 +7,14 @@ const eCommerceClient = axios.create({
   baseURL: 'http://a771792005a2b4fc3be50a71e9f3c835-1575173433.us-east-1.elb.amazonaws.com:5000/',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin':
+      'http://aac53e81081b042258fc80efa94a009c-104931072.us-east-1.elb.amazonaws.com:3000/',
   },
-});
+})
 
 export interface eCommerceApiResponse {
-  status: number;
-  payload: any;
+  status: number
+  payload: any
 }
 
-export default eCommerceClient;
+export default eCommerceClient
