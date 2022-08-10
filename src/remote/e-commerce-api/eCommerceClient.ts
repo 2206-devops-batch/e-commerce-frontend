91fb2114b06891a32e559a60d673bbef62ab3831
Christopher Barnes <chris.barnes.2000@me.com>
2022-08-10 13:54:48 -0700

    [Update]: CorsOrigin

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
 const eCommerceClient = axios.create({
   withCredentials: true,
   // baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
-  baseURL: 'http://af8e437071b9d4a8e94eced60a43d6b6-1814642247.us-east-1.elb.amazonaws.com:5000',
+  baseURL: 'http://adebcf51e12fe4acc9f4deac43840e35-1727142081.us-east-1.elb.amazonaws.com',
   headers: {
     'Content-Type': 'application/json',
     // 'Access-Control-Allow-Origin': 'http://localhost:3000',
