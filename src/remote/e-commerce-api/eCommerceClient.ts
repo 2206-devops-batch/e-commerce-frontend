5fc4a2aa8507f9f04b6b38a563b25d286cc2d4bc
Christopher Barnes <chris.barnes.2000@me.com>
2022-08-10 15:21:51 -0700

    [Update]: CrossDomain

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -3,15 +3,13 @@ import axios from 'axios'
 // This is the configuration for sending HTTP Requests with Axios
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
-  // withCredentials: true,
-  // // baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
+  withCredentials: true,
   baseURL: 'http://adebcf51e12fe4acc9f4deac43840e35-1727142081.us-east-1.elb.amazonaws.com:5000',
   headers: {
     'Content-Type': 'application/json',
-  //   // 'Access-Control-Allow-Origin': 'http://localhost:3000',
-  //   // 'Access-Control-Allow-Origin': 'http://a541fc074149e4506816882a4c7ef8e6-623895046.us-east-1.elb.amazonaws.com:3000',
-  //   'Access-Control-Allow-Origin': 'http://adebcf51e12fe4acc9f4deac43840e35-1727142081.us-east-1.elb.amazonaws.com:5000',
-  //   'Access-Control-Allow-Methods': '*',
+    crossDomain: true,
+    //   'Access-Control-Allow-Origin': 'http://adebcf51e12fe4acc9f4deac43840e35-1727142081.us-east-1.elb.amazonaws.com:5000',
+    //   'Access-Control-Allow-Methods': '*',
   },
 })
 
