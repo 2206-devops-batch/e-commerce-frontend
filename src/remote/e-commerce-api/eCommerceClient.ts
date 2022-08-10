3610a12c9747410e0bdba2e7d7069bf0d9cda763
Christopher Barnes <chris.barnes.2000@me.com>
2022-08-10 10:50:32 -0700

    [Update]: Simplify CrossOrign

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -2,24 +2,13 @@ import axios from 'axios'
 
 // This is the configuration for sending HTTP Requests with Axios
 // Very simple, but it also doesn't give us much abstraction
-// const eCommerceClient = axios.create({
-//   withCredentials: true,
-//   baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': 'http://localhost:3000',
-//   },
-// })
-
 const eCommerceClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://af8e437071b9d4a8e94eced60a43d6b6-1814642247.us-east-1.elb.amazonaws.com:5000/',
+  // baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
+  baseURL: 'http://af8e437071b9d4a8e94eced60a43d6b6-1814642247.us-east-1.elb.amazonaws.com:5000',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin':
-      'http://aac53e81081b042258fc80efa94a009c-104931072.us-east-1.elb.amazonaws.com:3000/',
-    'Access-Control-Allow-Headers': 'Content-Type, application/json',
-    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
   },
 })
 
